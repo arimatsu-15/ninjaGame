efa2c5364698d905fae40ecf12bf2dea36126d20
Arimatsu Masaaki <arimatsu15@gmail.com>
2019-02-07 18:46:48 +0900

    0207.1900

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,14 +4,54 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
+    [SerializeField]
+    private Transform _RightHandAnchor;
+
+    [SerializeField]
+    private Transform _LeftHandAnchor;
+
+    [SerializeField]
+    private Transform _CenterEyeAnchor;
+
+    [SerializeField]
+    private float _MaxDistance = 100.0f;
+
+    [SerializeField]
+    private LineRenderer _LaserPointerRenderer;
+
+    private Transform Pointer
+    {
+        get
+        {
+            // 現在アクティブなコントローラーを取得
+            var controller = OVRInput.GetActiveController();
+            if (controller == OVRInput.Controller.RTrackedRemote)
+            {
+                return _RightHandAnchor;
+            }
+            else if (controller == OVRInput.Controller.LTrackedRemote)
+            {
+                return _LeftHandAnchor;
+            }
+            // どちらも取れなければ目の間からビームが出る
+            return _CenterEyeAnchor;
+        }
+    }
+
     public GameObject player;
     public GameObject kagiPrefab;
     public GameObject kaginawaPrefab;
+    private GameObject kagiInstance;
+    private GameObject kaginawaInstance;
 
     public Vector3 playerPos;
     public Vector3 kaginawaPos;
-    public Vector3 directionPK;
-    public float distancePK;
+    private Vector3 directionPK;
+    private float distancePK;
+
+
+
+    public float kagiPower = 200f;
 
     public enum PLAYER_STATE
     {
@@ -33,12 +73,35 @@ public class GameManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+        var pointer = Pointer;
+        if (pointer == null || _LaserPointerRenderer == null)
+        {
+            return;
+        }
+        // コントローラー位置からRayを飛ばす
+        Ray pointerRay = new Ray(pointer.position, pointer.forward);
+
+        // レーザーの起点
+        _LaserPointerRenderer.SetPosition(0, pointerRay.origin);
+
+        RaycastHit hitInfo;
+        if (Physics.Raycast(pointerRay, out hitInfo, _MaxDistance))
+        {
+            // Rayがヒットしたらそこまで
+            _LaserPointerRenderer.SetPosition(1, hitInfo.point);
+        }
+        else
+        {
+            // Rayがヒットしなかったら向いている方向にMaxDistance伸ばす
+            _LaserPointerRenderer.SetPosition(1, pointerRay.origin + pointerRay.direction * _MaxDistance);
+        }
 
 
 
-        switch(playerState){
+        switch (playerState){
             case PLAYER_STATE.NOMAL:
                 if(OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger)){
+                    Shot(pointer);
                     playerState = PLAYER_STATE.THROW;
                 }
                 break;
@@ -59,5 +122,11 @@ public class GameManager : MonoBehaviour {
         }
   	}
 
+    public void Shot(Transform pointer)
+    {
+        kagiInstance = Instantiate(kagiPrefab, pointer.position, pointer.rotation) as GameObject;
+        kagiInstance.GetComponent<Rigidbody>().AddForce(kagiInstance.transform.forward * kagiPower);
+
+    }
 
 }
